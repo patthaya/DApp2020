2569241424fffbf406821eafe35c299b3d5e730e
Patthaya Tupthong <poon.cpc@gmail.com>
2020-11-21 16:29:23 +0700

    first commit

---
+++ 04_Web_DApp/contracts/Adoption.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+contract Adoption {
+    address[16] public adopters;
+
+    function adopt(uint petId) public returns (uint) {
+        require(petId >= 0 && petId <= 15);
+        adopters[petId] = msg.sender;
+        return petId;
+    }
+    function getAdopters() public view returns (address[16] memory){
+        return adopters;      
+    }
+    // function getAdopter(uint petId) public view return (address) {
+    //     require(petId >= 0 && petId <= 15);
+    //     return adopters[petId];
+    // }
+}
